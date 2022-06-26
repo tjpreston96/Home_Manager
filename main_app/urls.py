3ff4cdc56de0c72d37ff1952fc7df03e1c33bb70
Tyler Preston <tpreston96@gmail.com>
2022-06-25 20:51:53 -0500

    remove unecessary code

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,4 @@
-from django.urls import URLPattern, path
-from django.conf import settings
+from django.urls import path
 
 from . import views
 
@@ -33,7 +32,6 @@ urlpatterns = [
     path("shopping/", views.shopping_index, name="shopping_index"),
     path("shopping/create/", views.add_item, name="add_item"),
     path("shopping/<int:shopping_id>/delete/", views.delete_item, name="delete_item"),
-    # path("shopping/create/", views.ShoppingCreate.as_view(), name="shopping_create"),
     # ===== Notes =====
     path("notes/", views.notes_index, name="notes_index"),
     path("notes/<int:note_id>/", views.notes_detail, name="notes_detail"),
@@ -41,6 +39,4 @@ urlpatterns = [
     # path("notes/create/add/", views.add_note, name="add_note"),
     path("notes/<int:pk>/update/", views.NoteUpdate.as_view(), name="notes_update"),
     path("notes/<int:pk>/delete/", views.NoteDelete.as_view(), name="notes_delete"),
-    # ===== Accounts =====
-    path("accounts/signup/", views.signup, name="signup"),
 ]
