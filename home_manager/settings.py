c74420e2c303903ea9b9ceb36ba275d4b2c81b52
Tyler Preston <tpreston96@gmail.com>
2022-06-25 20:49:22 -0500

    remove unnecessary code

--- home_manager/settings.py
+++ home_manager/settings.py
@@ -10,15 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+# import os
 from pathlib import Path
+
+import django_heroku
 import environ
 
-
-# from decouple import config
 env = environ.Env()
 environ.Env.read_env()
 
-# import main_app
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,12 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATIC_ROOT
+
+# # STATIC_ROOT
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# # MEDIA_ROOT
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-import django_heroku
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
