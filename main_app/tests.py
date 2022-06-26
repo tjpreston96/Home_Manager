936e45639d3e8b13549199aca629c82d8596c288
Tyler Preston <tpreston96@gmail.com>
2022-06-25 20:46:30 -0500

    setUp for PlantTestCase

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import Client, RequestFactory, TestCase
+
+from .models import Plant, User
+from .views import *
+
+c = Client()
 
 # Create your tests here.
+class PlantTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username="Test User", email="test123@email.com", password="top_secret"
+        )
